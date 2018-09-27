7278117f46f958e7ccbece795ee3d569deb62958
SolarEclipse28 <areon_blade@yahoo.com>
2018-09-27 12:28:52 -0700

    Added More Souls

---
+++ Items/Souls/Normal/BlueSlimeSoul.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Tervania.Items.Souls.Normal {
+    public class BlueSlimeSoul : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Blue Slime's Soul");
+            Tooltip.SetDefault("Grants Higher Jumps"/* +
+                "\n+5% Move Speed"*/);
+        }
+
+        public override void SetDefaults() {
+            item.width = 22;
+            item.height = 20;
+            item.value = Item.buyPrice(0, 0, 10, 0);
+            item.rare = 2;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
+            player.jumpSpeedBoost += 2;
+        }
+    }
+}
---
+++ Items/Souls/Normal/PurpleSlimeSoul.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Tervania.Items.Souls.Normal {
+    public class PurpleSlimeSoul : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Purple Slime's Soul");
+            Tooltip.SetDefault("Safer Falls"/* +
+                "\n+5% Move Speed"*/);
+        }
+
+        public override void SetDefaults() {
+            item.width = 22;
+            item.height = 20;
+            item.value = Item.buyPrice(0, 0, 10, 0);
+            item.rare = 2;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
+            player.extraFall = 3;
+        }
+    }
+}
