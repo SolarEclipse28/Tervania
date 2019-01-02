7e03f79f4c3c88d8160ab66ebeaf463d3e46896e
SolarEclipse28 <areon_blade@yahoo.com>
2019-01-02 08:56:03 -0800

    Tweaked Pinky+ Skeletron Soul stats

--- Items/Souls/Boss/SkeletronSoul.cs
+++ Items/Souls/Boss/SkeletronSoul.cs
@@ -5,23 +5,17 @@ using Terraria.ModLoader;
 
 namespace Tervania.Items.Souls.Boss {
     public class SkeletronSoul : EnchantedSoul {
-        public SkeletronSoul() : base(2, Item.buyPrice(0, 0, 10, 0), "Skeletron", "Lower max mana but +10% damage", true) { }
+        public SkeletronSoul() : base(2, Item.buyPrice(0, 0, 10, 0), "Skeletron", "25% increased Mana cost but +15% magic damage", true) { }
 
         public override void Update(Player player) {
-            if (player.statMana >(player.statManaMax2 * 0.5)) {
-                player.statMana -= 1;
-            }
-            player.meleeDamage *= 1.1f;
-            player.magicDamage *= 1.1f;
-            player.minionDamage *= 1.1f;
-            player.rangedDamage *= 1.1f;
-            player.thrownDamage *= 1.1f;
+            player.manaCost *= 1.25f;
+            player.magicDamage *= 1.15f;
         }
     }
 
     public class SkeletronSoulDrop : GlobalNPC {
         public override void NPCLoot(NPC npc) {
-            if (npc.TypeName == "Skeletron") TervaniaUtils.DropItem(npc, 10f, mod.ItemType<Items.Souls.Boss.SkeletronSoul>());
+            if (npc.TypeName == "Skeletron") TervaniaUtils.DropItem(npc, 5f, mod.ItemType<Items.Souls.Boss.SkeletronSoul>());
         }
     }
 }
